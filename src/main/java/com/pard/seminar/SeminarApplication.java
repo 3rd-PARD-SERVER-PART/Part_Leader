79d3fcaa3038631b2c7769cbdc965eac08f848e9
KimRPG <hunsung9909@gmail.com>
2024-03-23 22:57:17 +0900

    .

--- src/main/java/com/pard/seminar/SeminarApplication.java
+++ src/main/java/com/pard/seminar/SeminarApplication.java
@@ -3,6 +3,7 @@ package com.pard.seminar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SeminarApplication {
 
